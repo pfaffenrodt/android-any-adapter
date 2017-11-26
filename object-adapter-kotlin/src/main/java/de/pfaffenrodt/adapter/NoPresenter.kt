100791dcd9b80d1390bee7ff7c254414f653ddf2
Pfaffenrodt <dimitri@pfaffenrodt.de>
2017-11-27 00:19:48 +0100

    [DOC] Add doc to NoPresenter and add license info.

--- object-adapter-kotlin/src/main/java/de/pfaffenrodt/adapter/NoPresenter.kt
+++ object-adapter-kotlin/src/main/java/de/pfaffenrodt/adapter/NoPresenter.kt
@@ -1,10 +1,24 @@
+/*
+ * Copyright (C) 2017 Dimitri Pfaffenrodt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package de.pfaffenrodt.adapter
 
 import android.view.View
 import android.view.ViewGroup
 
 /**
- * Created by Dimitri Pfaffenrodt on Oktober.2017.
+ * Simple Placeholder if no Presenter matched.
+ * Placeholder to support using not null properties.
  */
 class NoPresenter : Presenter() {
 
