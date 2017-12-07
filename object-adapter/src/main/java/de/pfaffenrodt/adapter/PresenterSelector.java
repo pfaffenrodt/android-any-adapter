53177ac4ff5ccfb8c162e5fde03e1d6336a6a414
Pfaffenrodt <dimitri@pfaffenrodt.de>
2017-12-07 22:49:25 +0100

    [TASK] update PresenterSelector. removed some whitespaces

--- object-adapter/src/main/java/de/pfaffenrodt/adapter/PresenterSelector.java
+++ object-adapter/src/main/java/de/pfaffenrodt/adapter/PresenterSelector.java
@@ -19,12 +19,10 @@ package de.pfaffenrodt.adapter;
  * Similar to {@link Presenter},  PresenterSelector is stateless.
  */
 public abstract class PresenterSelector {
-
     /**
      * Returns a presenter for the given item.
      */
     public abstract Presenter getPresenter(Object item);
-
     /**
      * Returns an array of all possible presenters.  The returned array should
      * not be modified.
--- object-adapter/src/main/java/de/pfaffenrodt/adapter/SinglePresenterSelector.java
+++ object-adapter/src/main/java/de/pfaffenrodt/adapter/SinglePresenterSelector.java
@@ -18,22 +18,18 @@ package de.pfaffenrodt.adapter;
  * A {@link PresenterSelector} that always returns the same {@link Presenter}.
  * Useful for rows of items of the same type that are all rendered the same way.
  */
-public class SinglePresenterSelector extends PresenterSelector {
-
+public final class SinglePresenterSelector extends PresenterSelector {
     private final Presenter mPresenter;
-
     /**
      * @param presenter The Presenter to return for every item.
      */
     public SinglePresenterSelector(Presenter presenter) {
         mPresenter = presenter;
     }
-
     @Override
     public Presenter getPresenter(Object item) {
         return mPresenter;
     }
-
     @Override
     public Presenter[] getPresenters() {
         return new Presenter[]{mPresenter};
