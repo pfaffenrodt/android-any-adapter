463f6d85a0441beb945eb8b17cfb1552babe322b
Pfaffenrodt <dimitri@pfaffenrodt.de>
2021-07-29 20:55:40 +0200

    :rocket: bump version 1.5.2

--- any-adapter/build.gradle.kts
+++ any-adapter/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "de.pfaffenrodt"
-version  = "1.5.1"
+version  = "1.5.2"
 val siteUrl = "https://github.com/pfaffenrodt/android-any-adapter"
 val gitUrl = "https://github.com/pfaffenrodt/android-any-adapter.git"
 
