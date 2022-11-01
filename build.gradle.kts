ecb197c934ef37e7ca773439d0f21e7022dd2f60
Pfaffenrodt <dimitri@pfaffenrodt.de>
2022-11-01 11:40:48 +0100

    ⬆️upgrade gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.3")
+        classpath("com.android.tools.build:gradle:7.3.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
         classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.7.20")
