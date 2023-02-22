57f6b9df7f0a8a6564749b6480482f1412f5c4ab
Pfaffenrodt <dimitri@pfaffenrodt.de>
2023-02-22 20:04:25 +0100

    ⬆️upgrade gradle, kotlin 1.8.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
+        classpath("com.android.tools.build:gradle:7.4.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
         classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.7.20")
     }
